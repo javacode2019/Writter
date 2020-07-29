b8ce97bd9801c5c1812c209dd94c55dbaf874476
Vytautas <vytautas.1717@gmail.com>
2020-07-30 02:39:08 +0300

    add database

--- src/main/java/lt/vytautas/writter/controllers/Greeting.java
+++ src/main/java/lt/vytautas/writter/controllers/Greeting.java
@@ -1,23 +1,38 @@
 package lt.vytautas.writter.controllers;
 
+import lt.vytautas.writter.domain.Message;
+import lt.vytautas.writter.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class Greeting {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Spring boot") String name,
                            Map<String,Object> model) {
         model.put("name", name);
         return "greeting";
     }
-    @GetMapping("/main")
+    @GetMapping
     public  String main(Map<String, Object> model){
-        model.put("some","Hello, coding is very fun!!!!");
+        Iterable<Message> messages= messageRepo.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+    @PostMapping
+    public String add (@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
+        Message message = new Message(text,tag);
+        messageRepo.save(message);
+        Iterable<Message> messages= messageRepo.findAll();
+        model.put("messages",messages);
         return "main";
     }
 
