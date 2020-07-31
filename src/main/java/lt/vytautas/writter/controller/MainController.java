eb8e9aa8c967cdd53337de6238663110352b99c3
Vytautas <vytautas.1717@gmail.com>
2020-07-31 18:24:05 +0300

    add user author

--- src/main/java/lt/vytautas/writter/controller/MainController.java
+++ src/main/java/lt/vytautas/writter/controller/MainController.java
@@ -1,8 +1,10 @@
 package lt.vytautas.writter.controller;
 
 import lt.vytautas.writter.domain.Message;
+import lt.vytautas.writter.domain.User;
 import lt.vytautas.writter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +23,11 @@ public class MainController {
         return "main";
     }
     @PostMapping("/main")
-    public String add (@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
-        Message message = new Message(text,tag);
+    public String add (
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String,Object> model){
+        Message message = new Message(text,tag,user);
         messageRepo.save(message);
         Iterable<Message> messages= messageRepo.findAll();
         model.put("messages",messages);
